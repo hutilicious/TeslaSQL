00e604d2ab71cbb311a348ea6011bcc5e0f9578e
Huti <huti1990@gmail.com>
2017-04-29 21:02:41 +0200

    init TeslaSQL

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,13 +11,14 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({ width: 800, height: 600, icon: __dirname + '/assets/icons/teslasql.png' })
+  mainWindow.setMinimumSize(800, 600);
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, '../html/index.html'),
     protocol: 'file:',
     slashes: true
   }))
