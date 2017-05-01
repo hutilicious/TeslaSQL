91312756a374653b4788bb6056465201c8cf2780
Huti <huti1990@gmail.com>
2017-05-01 18:19:53 +0200

    prepare Mysql connection and user settings

--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -1,15 +1,50 @@
 // Setup libs
 const $ = require('jquery');
+const mysql = require('mysql');
+const storage = require('electron-json-storage');
+
+/*storage.set('connection', { server: 'localhost', user: 'huti', password: 'huti' }, function (error) {
+    if (error) throw error;
+});*/
+
 require('jquery-resizable-dom');
 
 
 // Make panels resizeable
-$(".panel-left").resizable({
-    handleSelector: ".splitter",
+$("#teslasql-sidebar").resizable({
+    handleSelector: "#teslasql-resize-v",
     resizeHeight: false
 });
 
-$(".panel-container").resizable({
-    handleSelector: ".splitter-horizontal",
+$("#teslasql-panel-container").resizable({
+    handleSelector: "#teslasql-resize-h",
     resizeWidth: false
+});
+
+$("#mysql-test").click(function () {
+    storage.get('connection', function (error, config) {
+        if (error) throw error;
+
+        var connection = mysql.createConnection({
+            host: config.server,
+            user: config.user,
+            password: config.password
+        });
+        connection.connect();
+        connection.query('SHOW DATABASES', function (error, results, fields) {
+            if (error) throw error;
+
+            var arrDbs = [];
+            results.forEach(function (element) {
+                arrDbs.push(element.Database);
+            });
+            arrDbs.sort();
+            arrDbs.forEach(function (database) {
+                $("#teslasql-sidebar").append('<div>' + database + '</div>');
+            });
+        });
+
+        connection.end();
+    });
+
 });
