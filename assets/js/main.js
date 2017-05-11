caba12b86741b7bea8f3499446c15339e9faf1d9
Carsten Rothhaß <crothhass@megabild.de>
2017-05-11 16:02:37 +0200

    make query highlight while user is typing, add query formatting

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,8 @@ const electronLocalshortcut = require('electron-localshortcut');
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow() {
+function createWindow()
+{
   // Create the browser window.
   mainWindow = new BrowserWindow({ width: 1200, height: 800, icon: __dirname + '/../icons/teslasql.png' })
   mainWindow.setMinimumSize(700, 500);
@@ -25,18 +26,27 @@ function createWindow() {
     slashes: true
   }))
 
-
-
-  electronLocalshortcut.register(mainWindow, 'F9', () => {
-    console.log('You pressed F9');
+  electronLocalshortcut.register(mainWindow, 'F9', () =>
+  {
     mainWindow.webContents.send('shortcut-pressed', 'F9');
+  });
+
+  electronLocalshortcut.register(mainWindow, 'F8', () =>
+  {
+    mainWindow.webContents.send('shortcut-pressed', 'F8');
+  });
+
+  electronLocalshortcut.register(mainWindow, 'F7', () =>
+  {
+    mainWindow.webContents.send('shortcut-pressed', 'F7');
   });
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function()
+  {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -50,18 +60,22 @@ function createWindow() {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function()
+{
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (process.platform !== 'darwin')
+  {
     app.quit()
   }
 })
 
-app.on('activate', function () {
+app.on('activate', function()
+{
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (mainWindow === null)
+  {
     createWindow()
   }
 })
--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -1,10 +1,12 @@
 // Setup libs
 const $ = require('jquery');
 require('jquery-resizable-dom');
-//require('jquery-table-fixed-header');
+const hljs = require('highlightjs');
+const rangy = require('rangy/lib/rangy-selectionsaverestore.js');
 const Handsontable = require('handsontable');
+const striptags = require('striptags');
 const mysql = require('mysql');
-const sqlformatter = require('sql-formatter');
+const sqlformatter = require('sql-formatter')
 const storage = require('electron-json-storage');
 const shortcutListener = require('electron').ipcRenderer;
 let connection = null;
@@ -18,12 +20,15 @@ let datagrid = null;
 });*/
 
 // This is where we put our code, just for the hell of it
-class TeslaSql {
+class TeslaSql
+{
     /**
      * constructer cant be private, so we hack it a bit
      */
-    constructor(singletonToken) {
-        if (_singleton !== singletonToken) {
+    constructor(singletonToken)
+    {
+        if (_singleton !== singletonToken)
+        {
             throw new Error('Cannot instantiate directly.');
         }
     }
@@ -32,8 +37,10 @@ class TeslaSql {
      * returns an instance to TeslaSql
      * @returns TeslaSql
      */
-    static getInstance() {
-        if (!this[_singleton]) {
+    static getInstance()
+    {
+        if (!this[_singleton])
+        {
             this[_singleton] = new TeslaSql(_singleton);
         }
 
@@ -43,9 +50,11 @@ class TeslaSql {
     /**
      * entry point, init gui
      */
-    init() {
+    init()
+    {
         this.resize();
-        this.connect(function () {
+        this.connect(function()
+        {
             TeslaSql.getInstance().query('SHOW full processlist');
         });
         this.listenToShortcuts();
@@ -77,22 +86,27 @@ class TeslaSql {
             onDragEnd: this.resize
         });
 
-        $('#teslasql-cmd').focus();
+        $('#query').focus();
+        $('#query').keyup(this.highlightQuery);
     }
     /**
      * calculate the height of the panels
      */
-    resize() {
-        if (datagrid !== null) {
+    resize()
+    {
+        if (datagrid !== null)
+        {
             datagrid.updateSettings({ height: $('#results').height(), width: $('#results').width() });
         }
     }
     /**
      * starts connection to database
      */
-    connect(callbackfnc) {
+    connect(callbackfnc)
+    {
         var _this = this;
-        storage.get('connection', function (error, config) {
+        storage.get('connection', function(error, config)
+        {
             if (error) throw error;
 
             connection = mysql.createConnection({
@@ -101,52 +115,99 @@ class TeslaSql {
                 password: config.password
             });
             connection.connect();
-            if ($.isFunction(callbackfnc)) {
+            if ($.isFunction(callbackfnc))
+            {
                 callbackfnc();
             }
-            _this.query('SHOW DATABASES', function (results, fields) {
+            _this.query('SHOW DATABASES', function(results, fields)
+            {
                 var arrDbs = [];
-                results.forEach(function (element) {
+                results.forEach(function(element)
+                {
                     arrDbs.push(element.Database);
                 });
                 arrDbs.sort();
-                arrDbs.forEach(function (database) {
+                arrDbs.forEach(function(database)
+                {
                     $sidebar.append('<div>' + database + '</div>');
                 });
             });
         });
     }
+
+    sanitizeQuery(query)
+    {
+        query = query.replace(/<br>/g, "\n");
+        query = striptags(query);
+        return query;
+    }
+
+    formatQuery()
+    {
+        var savedSel = rangy.saveSelection();
+        var elem = $("#query");
+        var savedSelElem = elem.find('.rangySelectionBoundary');
+        var savedSelElemHtml = savedSelElem[0].outerHTML;
+        savedSelElem.replaceWith('_TESLASQL_QUERY_CURSOR_');
+        var query = this.sanitizeQuery($("#query").html());
+        var formatted = sqlformatter.format(query);
+        elem.html(formatted);
+        elem.html(elem.html().replace('_TESLASQL_QUERY_CURSOR_', savedSelElemHtml));
+        rangy.restoreSelection(savedSel);
+        this.highlightQuery();
+    }
+
+    highlightQuery()
+    {
+        var savedSel = rangy.saveSelection();
+        var html = $("#query");
+        html.find('.hljs-keyword').replaceWith(function() { return this.innerHTML; });
+        html.find('font').replaceWith(function() { return this.innerHTML; });
+        $("#query").each(function(i, block)
+        {
+            hljs.highlightBlock(block);
+        });
+
+        rangy.restoreSelection(savedSel);
+    }
+
     /**
      * runs a query with current connection
      * @param string query 
      * @param function callbackfnc 
      */
-    query(query, callbackfnc) {
+    query(query, callbackfnc)
+    {
         var _this = this;
-        const astObj = sqlformatter.format(query);
-        console.log(astObj);
-        $log.append('<div>' + query + ';</div>');
-        connection.query(query, function (error, results, fields) {
-            try {
-                if (error) {
+        $log.append('<div>' + sqlformatter.format(query) + ';</div>');
+        connection.query(query, function(error, results, fields)
+        {
+            try
+            {
+                if (error)
+                {
                     throw error;
                 }
-                if ($.isFunction(callbackfnc)) {
+                if ($.isFunction(callbackfnc))
+                {
                     callbackfnc(results, fields);
-                } else {
-                    if ($.isArray(results)) {
+                } else
+                {
+                    if ($.isArray(results))
+                    {
                         $("#results").html('<div id="results--datagrid"></div>');
 
                         var container = document.getElementById('results--datagrid');
-                        if (datagrid !== null) {
+                        if (datagrid !== null)
+                        {
                             datagrid.destroy();
                         }
                         datagrid = new Handsontable(container, {
                             data: results,
                             minSpareCols: 0,
                             fillHandle: false,
-                            colHeaders: fields.map(function (elem) { return elem.name }),
-                            columns: fields.map(function (elem) { return { data: elem.name, editor: 'text' } }),
+                            colHeaders: fields.map(function(elem) { return elem.name }),
+                            columns: fields.map(function(elem) { return { data: elem.name, editor: 'text' } }),
                             minSpareRows: 0,
                             rowHeaders: false,
                             contextMenu: false,
@@ -158,11 +219,14 @@ class TeslaSql {
 						/*$("#teslasql-main-results table").containerTableFixedHeader({
 							scrollContainer: $("#teslasql-main-results")
 						});*/
-                    } else {
+                    } else
+                    {
                         $log.append('<div>' + JSON.stringify(results) + '</div>');
                     }
                 }
-            } catch (err) {
+            } catch (err)
+            {
+                console.log(err);
                 $log.append('<div>' + err + '</div>');
             }
             $log.scrollTop($log[0].scrollHeight);
@@ -171,12 +235,23 @@ class TeslaSql {
     /**
      * handle shortcut events registered in main.js
      */
-    listenToShortcuts() {
+    listenToShortcuts()
+    {
         var _this = this;
-        shortcutListener.on('shortcut-pressed', function (event, arg) {
-            if (arg == "F9") {
-                var query = $("#query").val();
+        shortcutListener.on('shortcut-pressed', function(event, arg)
+        {
+            if (arg == "F9")
+            {
+                var query = _this.sanitizeQuery($("#query").html());
                 _this.query(query);
+            }
+            else if (arg == "F8")
+            {
+                _this.formatQuery();
+            }
+            else if (arg == "F7")
+            {
+                _this.highlightQuery();
             }
         });
     }
